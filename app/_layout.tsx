f24e3afa07be4586b0ba03d5cee4bab939f9cf97
Brylle <miguelbrylle@gmail.com>
2026-03-06 06:31:47 -0500

    setup server state (tanstack query), added axios with basic query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import { queryClient } from "./react-query/queryClient";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <QueryClientProvider client={queryClient}>
+    <Stack />
+  </QueryClientProvider>
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,43 @@
-import { Text, View } from "react-native";
+import { QueryFunctionContext, useQuery } from "@tanstack/react-query";
+import axios from 'axios';
+import { useState } from "react";
+import { Button, ScrollView, Text, TextInput, View } from "react-native";
 
 export default function Index() {
+
+  const [pokemon, setPokemon] = useState<string>("")
+  const [userPokemonChoice, setUserPokemonChoice] = useState("")
+
+  const fetchPokemon = async ({ queryKey, signal }: QueryFunctionContext<[string, string]>) => {
+    const [_key, pokemonName] = queryKey;
+    const res = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`,
+      { signal }
+    );
+    return res.data;
+  };
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['pokemon', pokemon],
+    queryFn: fetchPokemon,
+    enabled: !!pokemon,
+    staleTime: 30 * 60 * 1000, // 30 mins
+  });
+
+  console.log("pokemon:", pokemon)
+  console.log("data name:", data?.name)
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView>
+      <View>{isLoading ? <Text>loading...</Text> : error ? <Text>error occured.</Text> : <Text></Text>}
+      </View>
+      <View>
+        <TextInput style={{ color: '#fff', backgroundColor: '#333', padding: 10 }} placeholder="e.g. Pikachu" value={userPokemonChoice} onChangeText={setUserPokemonChoice} />
+        <Button title="Search Pokemon" onPress={() => setPokemon(userPokemonChoice.toLowerCase())} />
+      </View>
+      <View>
+        <Text>{data?.name}</Text>
+      </View>
+    </ScrollView>
   );
 }
