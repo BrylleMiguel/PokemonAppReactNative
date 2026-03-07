ed0fc23f5e36dbd0b4be2feeafcc57a2f2982d3b
Brylle <miguelbrylle@gmail.com>
2026-03-07 05:30:01 -0500

    refactored, implement to fetch pokemons

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,57 @@
+
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { ScrollView } from "react-native";
-import * as z from 'zod';
+import { Image, ScrollView, Text, View } from "react-native";
+import * as z from "zod";
 
-const resultSchema = z.object({ name: z.string(), url: z.string() })
-const pokemonsSchema = z.object({ count: z.number(), next: z.string(), previous: z.string(), results: z.array(resultSchema) })
-type Pokemons = z.infer<typeof pokemonsSchema>
+const PokemonDetailSchema = z.object({
+	name: z.string(),
+	sprites: z.object({ front_default: z.string() }),
+});
+type PokemonDetail = z.infer<typeof PokemonDetailSchema>;
+
+const resultSchema = z.object({ name: z.string(), url: z.string() });
+const pokemonsSchema = z.object({
+	count: z.number(),
+	next: z.string().nullable(),
+	previous: z.string().nullable(),
+	results: z.array(resultSchema),
+});
 
 export default function Index() {
-	const fetchPokemons = async (): Promise<Pokemons> => {
-		const res = await axios.get('https://pokeapi.co/api/v2/pokemon')
-		return pokemonsSchema.parse(res.data)
-	}
+	// Fetch list + details
+	const fetchPokemons = async (): Promise<{ details: PokemonDetail[] }> => {
+		const res = await axios.get("https://pokeapi.co/api/v2/pokemon?offset=20&limit=20");
+		const pokemons = pokemonsSchema.parse(res.data);
+
+		// Fetch all details in parallel
+		const details = await Promise.all(
+			pokemons.results.map(async ({ url }) => {
+				const detailRes = await axios.get(url);
+				return PokemonDetailSchema.parse(detailRes.data);
+			})
+		);
+
+		return { details };
+	};
 
 	const { data, isLoading, error } = useQuery({
-		queryKey: ["pokemons"],
-		queryFn: () => fetchPokemons,
-		staleTime: 30 * 60 * 1000, // 30 mins
+		queryKey: ["pokemons"], // TanStack Query key
+		queryFn: fetchPokemons,
+		staleTime: 30 * 60 * 1000,
 	})
 
-	return <ScrollView>
+	console.log(data?.details)
 
+	if (isLoading) return <Text>Loading...</Text>;
+	if (error) return <Text>Error fetching Pokémon</Text>;
+
+	return <ScrollView>
+		{data?.details.map(p => (
+			<View>
+				<Text key={p.name}>{p.name}</Text>
+				<Image width={100} height={100} source={{ uri: p.sprites.front_default }} />
+			</View>
+		))}
 	</ScrollView>;
 }
-
